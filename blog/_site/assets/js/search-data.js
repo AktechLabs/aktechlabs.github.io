65129c95af98d063649ffb256b90d1f5e0b41a99
Amit Kumar <dtu.amit@gmail.com>
2020-07-04 14:24:44 +0100

    Fix pagination

--- blog/_site/assets/js/search-data.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Hello World",
-        "excerpt":"It took a while, but now we finally have a blog! ","categories": ["new"],
-        "tags": [],
-        "url": "http://localhost:4000/blog/new/2020/07/04/hello-world.html"
-      }]
